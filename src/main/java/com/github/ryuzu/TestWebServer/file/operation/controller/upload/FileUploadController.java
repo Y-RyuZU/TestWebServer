265f79b6b7f5c9ffeb45c31505ed9537ee5d54bd
Y-RyuZU <ryuzu1006@gmail.com>
2023-01-19 21:49:57 +0900

    init

--- src/main/java/com/github/ryuzu/TestWebServer/file/operation/controller/upload/FileUploadController.java
+++ src/main/java/com/github/ryuzu/TestWebServer/file/operation/controller/upload/FileUploadController.java
@@ -6,10 +6,7 @@ import com.github.ryuzu.TestWebServer.file.operation.models.log.FileOperationLog
 import com.github.ryuzu.TestWebServer.security.service.AccountDetails;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.*;
@@ -20,6 +17,7 @@ public class FileUploadController {
     private final FileUploadService service;
 
     @PostMapping("api/files/upload")
+    @CrossOrigin("http://localhost:5173")
     public void upload(
             @AuthenticationPrincipal AccountDetails details,
             @RequestParam String path,
